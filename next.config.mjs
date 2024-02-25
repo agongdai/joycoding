432152edb0d997bb53ea978a506d0b12772bb45b
shaojiang <caishaojiang@gmail.com>
2024-02-25 10:09:51 +0800

    remove sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs';
 import remarkGfm from 'remark-gfm';
 
 import createMDX from '@next/mdx';
@@ -44,34 +43,6 @@ const withMDX = createMDX({
   },
 });
 
-const config = process.env.NODE_ENV === 'production'
-  ? withSentryConfig(withMDX(nextConfig), {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: 'jsesio',
-    project: 'myex',
-  }, {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: false,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  })
-  : withMDX(nextConfig);
+const config = withMDX(nextConfig);
 
 export default config;
