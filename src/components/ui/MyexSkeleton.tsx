791db428ce706d7543239d22c324717ef5bbe7d4
shaojiang <caishaojiang@gmail.com>
2024-07-10 16:13:00 +0800

    Minor skeleton UI

--- src/components/ui/MyexSkeleton.tsx
+++ src/components/ui/MyexSkeleton.tsx
@@ -5,7 +5,7 @@ import Skeleton from '@mui/material/Skeleton';
 export default function MyexSkeleton() {
   return (
     <div>
-      {[1, 2, 3, 4, 5].map((v) => (
+      {new Array(10).fill(1).map((v) => (
         <div key={v} className='grid gap-4 grid-cols-9 my-4'>
           <Skeleton variant='rectangular' width='100%' height={50} className='col-span-3' />
           <Skeleton variant='rectangular' width='100%' height={50} />
