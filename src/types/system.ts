d4fd0c919e97bcf5bdc2c354c1c0004169cb2d1e
shaojiang <caishaojiang@gmail.com>
2024-07-23 10:19:35 +0800

    Removed unused user parameter

--- src/types/system.ts
+++ src/types/system.ts
@@ -1,7 +1,6 @@
 export enum SystemParameter {
   MiniSidebarOpen = 'dom.miniSidebarOpen',
   MobileSidebarOpen = 'dom.mobileSidebarOpen',
-  ScrollTop = 'dom.scrollTop',
   IsMobile = 'dom.isMobile',
   Theme = 'dom.theme',
   Favorites = 'trading.favorites',
@@ -16,7 +15,6 @@ export const EmptySystemParameterSettings = {} as SystemParameterSettings;
 export const DefaultSystemParameterSettings: SystemParameterSettings = {
   [SystemParameter.MiniSidebarOpen]: 'false',
   [SystemParameter.MobileSidebarOpen]: 'false',
-  [SystemParameter.ScrollTop]: '0',
   [SystemParameter.IsMobile]: 'false',
   [SystemParameter.Theme]: 'dark',
   [SystemParameter.Favorites]: '"[]"',
